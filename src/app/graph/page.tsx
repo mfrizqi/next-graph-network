636e056f42a3a20475dead18b61fc7cef8304a7a
mfrizqi <muhammadfajarrizqi@gmail.com>
2024-11-22 05:40:41 +0700

    remove unuse import & function

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -1,13 +1,6 @@
 "use client"
 import { useEffect, useRef, useState } from "react";
-import { Network, Node } from "vis-network";
-
-
-function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+import { Network } from "vis-network";
 
 export default function Graph() {
 
