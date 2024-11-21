983728d542222dc6ea3df1f5baeb93bffd54bdd4
mfrizqi <muhammadfajarrizqi@gmail.com>
2024-11-22 03:25:26 +0700

    swap mockapi to supabase, Fidgeting with network

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -1,49 +1,86 @@
 "use client"
 import { useEffect, useRef, useState } from "react";
-import { Network } from "vis-network";
+import { Network, Node } from "vis-network";
+
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export default function Graph() {
- 
-  const [networkData, setNetworkData] = useState([])
 
-  const nodes = [
-    { id: 1, label: "label 1" },
-    { id: 2, label: "label 2" },
-    { id: 3, label: "label 3" },
-    { id: 4, label: "label 4" },
-    { id: 5, label: "label 5" },
-  ];
-  const edges = [
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
-    { from: 3, to: 3 },
-  ];
+  const db = {
+    baseUrl: 'https://ffkvumomrhabgwbrvblq.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZma3Z1bW9tcmhhYmd3YnJ2YmxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyMTY1NjQsImV4cCI6MjA0Nzc5MjU2NH0.G5Y-c7j8TCHCPd25Cat_YquvD9RU_JaJPgMKHhmThJA'
+  }
 
-  const options = {}
+  const [nodes, setNodes] = useState([])
+  const [edges, setEdges] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const options = {
+    nodes: {
+      shape: "dot",
+      scaling:{
+        min: 1,
+        max: 1
+      },
+    }
+  }
 
   const NetworkRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    async function fetchPosts() {
-      let res = await fetch('https://673e87d1a9bc276ec4b4ba76.mockapi.io/network-graph/networks')
-      let data = await res.json()
-      setNetworkData(data)
-    }
+    if (nodes.length === 0) {
+      fetch(db.baseUrl + '/rest/v1/networks', {
+        method: 'GET',
+        headers: {
+          "Apikey": db.anonKey,
+          "Authorization": "Bearer " + db.anonKey
+        }
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data)
+          setNodes(data)
+          setIsLoading(false)
+        })
 
-    fetchPosts();
+      fetch(db.baseUrl + '/rest/v1/edges', {
+        method: 'GET',
+        headers: {
+          "Apikey": db.anonKey,
+          "Authorization": "Bearer " + db.anonKey
+        }
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data)
+          setEdges(data)
+          setIsLoading(false)
+        })
+    }
 
     const network =
       NetworkRef.current &&
-      new Network(NetworkRef.current, { networkData, edges }, options || {});
-      console.log(network)
-  }, [NetworkRef, networkData, edges]);
+      new Network(NetworkRef.current, { nodes, edges }, options || {});
+    console.log(network)
 
+    console.log(nodes)
+    console.log(edges)
+
+  }, [NetworkRef, nodes, edges]);
 
   return (
+
     <div className="p-10">
       <h1 className="text-xl">Graphs page</h1>
-      <div id="mynetwork" style={{backgroundColor:'white', height: '50vh'}} ref={NetworkRef}></div>
+      {isLoading ? (
+        <div className="border-2 border-zinc-600 bg-zinc-600 rounded-md text-gray-500 flex justify-center items-center" style={{ height: '80vh' }}>Loading Nodes...</div>
+      ) : (
+        <div id="mynetwork" className="border-2 border-zinc-600 rounded-md" style={{ backgroundColor: 'white', height: '80vh' }} ref={NetworkRef}></div>
+      )}
     </div>
   );
 }
