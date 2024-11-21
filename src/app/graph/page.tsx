a1a17a2629bbbbfd3e4dba6c38ff08eabba3e187
mfrizqi <muhammadfajarrizqi@gmail.com>
2024-11-21 09:19:15 +0700

    add config next and wip fetchin

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -1,15 +1,17 @@
 "use client"
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Network } from "vis-network";
 
 export default function Graph() {
+ 
+  const [networkData, setNetworkData] = useState([])
 
   const nodes = [
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" },
-    { id: 3, label: "Node 3" },
-    { id: 4, label: "Node 4" },
-    { id: 5, label: "Node 5" },
+    { id: 1, label: "label 1" },
+    { id: 2, label: "label 2" },
+    { id: 3, label: "label 3" },
+    { id: 4, label: "label 4" },
+    { id: 5, label: "label 5" },
   ];
   const edges = [
     { from: 1, to: 3 },
@@ -23,11 +25,19 @@ export default function Graph() {
 
   const NetworkRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
+    async function fetchPosts() {
+      let res = await fetch('https://673e87d1a9bc276ec4b4ba76.mockapi.io/network-graph/networks')
+      let data = await res.json()
+      setNetworkData(data)
+    }
+
+    fetchPosts();
+
     const network =
       NetworkRef.current &&
-      new Network(NetworkRef.current, { nodes, edges }, options || {});
+      new Network(NetworkRef.current, { networkData, edges }, options || {});
       console.log(network)
-  }, [NetworkRef, nodes, edges]);
+  }, [NetworkRef, networkData, edges]);
 
 
   return (
